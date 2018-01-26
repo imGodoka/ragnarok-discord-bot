f27bdb35d270bcfd74a4fb703612e20dac2397af
Pikachu <pikachu@evnts.com.br>
2018-01-26 10:00:52 -0200

    Porta

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
-}).listen(8080);
+}).listen(process.env.PORT);
 
 var app = express()
 
